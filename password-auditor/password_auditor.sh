0ee746936684a0aca3b74491714298ea94510f4f
haiaza <ajames1342@gmail.com>
2025-03-01 14:02:29 -0500

    Added Config - UnixEpoch for dynamic report naming

--- password-auditor/password_auditor.sh
+++ password-auditor/password_auditor.sh
@@ -9,3 +9,8 @@ SecurePass!2024" > weak_password_list.txt
 
 # Common passwords list (common_passwords.txt)
 curl -sO https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10k-most-common.txt
+
+#Configuration
+INPUT_FILE='weak_password.txt'
+COMMON_PASSWORDS='10k-most-common.txt'
+REPORT='password_report_$(date + %s)'
