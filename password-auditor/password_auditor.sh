008f28a200d3faf4ba0a651876879e6c0ebc69a1
haiaza <ajames1342@gmail.com>
2025-03-01 12:42:43 -0500

    Restored progress. Password Auditor

---
+++ password-auditor/password_auditor.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Passwords to test (weak_password_list.txt)
+echo "password123
+summer2023
+Admin#2024
+qwerty
+SecurePass!2024" > weak_password_list.txt
+
+# Common passwords list (common_passwords.txt)
+curl -sO https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10k-most-common.txt
